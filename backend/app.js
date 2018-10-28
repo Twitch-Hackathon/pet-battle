07f4ac469649307d6aaad4bfa7e760764edbc024
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-27 21:30:36 -0700

    Make backend runable

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const pet = require('/controllers/pet.js');
+const pet = require('./controllers/pet.js');
 
 const port = process.env.PORT || 5000;
 
--- backend/controllers/pet.js
+++ backend/controllers/pet.js
@@ -1,14 +1,15 @@
 const Pet = require('../models/pet.js');
+const https = require('https');
 const io = require('socket.io')(https);
 
 io.on('connection', (socket) => {
     socket.on('attack', (userId, channelId) => {
-        Pet.findOne(userId: userId)
+        Pet.findOne({userId: userId})
             .then((userPet) => {
-                Pet.findOne(userId: channelId)
+                Pet.findOne({userId: channelId})
                     .then((broadcasterPet) => {
                         broadcasterPet.set({
-                            health: broadcasterPet - (userPet.level * 10);
+                            health: broadcasterPet - (userPet.level * 10)
                         });
                         broadcasterPet.save().then((pet) => {
                             io.emit('attack', pet);
@@ -21,7 +22,7 @@ io.on('connection', (socket) => {
 module.exports = (app) => {
     app.get('/pet', (req, res) => {
         const userId = req.body.userId;
-        Pet.findOne(userId: userId)
+        Pet.findOne({userId: userId})
             .then((pet) => {
                 if(pet) {
                     res.send({level: pet.level})
@@ -39,7 +40,7 @@ module.exports = (app) => {
 
     app.get('/boss', (req, res) => {
         const userId = req.body.channelId;
-        Pet.findOne(userId: channelId)
+        Pet.findOne({userId: channelId})
             .then((pet) => {
                 if(pet) {
                     res.send({health: pet.health})
