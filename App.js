b4617fa8fb38f98a47ea4377c2c5e0e744b0bd8e
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-28 09:21:29 -0700

    Create heroku branch

--- App.js
+++ App.js
@@ -1,136 +1,203 @@
-import React from 'react'
-import Authentication from '../../util/Authentication/Authentication'
+const app = require('express')();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+// const PubSub = require('pubsub-js');
+// const rp = require('request-promise');
+// const WebSocket = require('ws');
+//
+// const ws = new WebSocket('wss://pubsub-edge.twitch.tv:443');
+//
+// var twitchClientCredentialToken = "";
+//
+// // Get twitch access token and listen to channel
+// getTwitchClientCredentials()
+//     .then(function(accessToken){
+//         // Start listening for subscription event
+//         var listenRequest = {
+//             "type": "LISTEN",
+//             // "nonce": Math.random()*42424242, //optional
+//             "data": {
+//                 "topics": ["channel-subscribe-events-v1.265737932"],
+//                 "auth_token": twitchClientCredentialToken
+//             }
+//         };
+//
+//         ws.send(JSON.stringify(listenRequest));
+//     });
+//
+// function getTwitchClientCredentials() {
+//     // POST https://id.twitch.tv/oauth2/token?client_id=uo6dggojyb8d6soh92zknwmi5ej1q2&client_secret=nyo51xcdrerl8z9m56w9w6wg&grant_type=client_credentials
+//     var options = {
+//         method: 'POST',
+//         uri: 'https://id.twitch.tv/oauth2/token?client_id=ljv7bvet8kgn92kbt1wem23z43fu6y&client_secret=ax3140sbtm4yztmpo3frazlcgx8q1a&grant_type=client_credentials',
+//         json: true // Automatically stringifies the body to JSON
+//     };
+//
+//     return rp(options)
+//         .then(function (parsedBody) {
+//             // POST succeeded...
+//             console.log(parsedBody)
+//             twitchClientCredentialToken = parsedBody.access_token;
+//             return twitchClientCredentialToken;
+//         })
+//         .catch(function (err) {
+//             // POST failed...
+//             console.log(err)
+//             return null;
+//         });
+// }
+//
+// ws.on('open', function open() {
+//     // ws.send('something');
+//     console.log('~~~~~opened websocket to twitch pubsub server')
+//     ws.send('{"type": "PING"}');
+//
+//     // Twitch requires a keepalive ping avery 5 minutes or else it closes the connection
+//     setTimeout(function(){
+//         console.log('~~~~~sending keepalive to twitch pubsub server');
+//         ws.send('{"type": "PING"}');
+//     }, 240000);
+// });
+// ws.on('close', function open() {
+//     // ws.send('something');
+//     console.log('~~~~~closed websocket to twitch pubsub server')
+// });
+//
+// ws.on('message', function incoming(data) {
+//     console.log('~~~~~received twitch pubsub data: ' + data);
+//     var jsonData = JSON.parse(data);
+//
+//     // Check if the message is a subscribe event
+//     // NOTE: to simulate this we can change it to a whispers event or something and send a whisper
+//     if(jsonData.data && jsonData.data.topic && jsonData.data.topic.indexOf('channel-subscribe-events-v1') == 0){
+//     // if(jsonData.data && jsonData.data.topic && jsonData.data.topic.indexOf('whispers') == 0){
+//         subscriptionEventHandler(jsonData);
+//     }
+// });
 
-import './App.css'
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-export default class App extends React.Component{
-    constructor(props){
-        super(props)
-        this.Authentication = new Authentication()
+const Pet = require('./models/pet.js');
 
-        //if the extension is running on twitch or dev rig, set the shorthand here. otherwise, set to null. 
-        this.twitch = window.Twitch ? window.Twitch.ext : null
-        this.state={
-            finishedLoading:false,
-            theme:'light',
-            isVisible:true,
-            bossName: 'Raid Boss',
-            bossAvatar: 'https://static-cdn.jtvnw.net/emoticons/v1/973/3.0',
-            bossMaxHP: 100,
-            bossHP: 100,
-            shake: false,
-            dead: false
-        }
+io.on('connection', (socket) => {
+    socket.on('attack', (userId, channelId) => {
+        console.log("USER:", userId)
+        console.log("CHANNEL", channelId)
+        Pet.findOne({userId: userId})
+            .then((userPet) => {
+                Pet.findOne({userId: channelId})
+                    .then((broadcasterPet) => {
+                        broadcasterPet.set({
+                            health: broadcasterPet.health - (userPet.level * 10)
+                        });
+                        broadcasterPet.save().then((pet) => {
+                            io.emit('attack', pet);
+                        })
+                    }).catch((err) => {
+                        console.error(err);
+                    });
+            });
+    });
+});
 
-        this.doDamage = this.doDamage.bind(this);
+function subscriptionEventHandler(data) {
+    /*
+    {
+       "type": "MESSAGE",
+       "data": {
+          "topic": "channel-subscribe-events-v1.44322889",
+          "message": {
+             "user_name": "dallas",
+             "display_name": "dallas",
+             "channel_name": "twitch",
+             "user_id": "44322889",
+             "channel_id": "12826",
+             "time": "2015-12-19T16:39:57-08:00",
+             "sub_plan": "Prime"/"1000"/"2000"/"3000",
+             "sub_plan_name": "Channel Subscription (mr_woodchuck)",
+             "months": 9,
+             "context": "sub"/"resub",
+             "sub_message": {
+                "message": "A Twitch baby is born! KappaHD",
+                "emotes": [
+                {
+                   "start": 23,
+                   "end": 7,
+                   "id": 2867
+                }]
+             }
+         }
+       }
     }
+     */
 
-    contextUpdate(context, delta){
-        if(delta.includes('theme')){
-            this.setState(()=>{
-                return {theme:context.theme}
+    var channelId = data.data.message.channel_id;
+    return Pet.findOne({userId: channelId})
+        .then((broadcasterPet) => {
+            console.log("~~~~~~Leveling up pet for channel: " + channelId);
+            broadcasterPet.set({
+                level: broadcasterPet.level + 1,
+                health: (broadcasterPet.level + 1) * 10
+            });
+            return broadcasterPet.save().then((pet) => {
+                return pet;
             })
-        }
-    }
+        }).catch((err) => {
+        console.error(err);
+        return err;
+    });
 
-    visibilityChanged(isVisible){
-        this.setState(()=>{
-            return {
-                isVisible
-            }
-        })
-    }
-
-    componentDidMount(){
-        if(this.twitch){
-            this.twitch.onAuthorized((auth)=>{
-                this.Authentication.setToken(auth.token, auth.userId)
-                if(!this.state.finishedLoading){
-                    // if the component hasn't finished loading (as in we've not set up after getting a token), let's set it up now.
-
-                    // now we've done the setup for the component, let's set the state to true to force a rerender with the correct data.
-                    this.setState(()=>{
-                        return {finishedLoading:true}
-                    })
-                }
-            })
-
-            this.twitch.listen('broadcast',(target,contentType,body)=>{
-                this.twitch.rig.log(`New PubSub message!\n${target}\n${contentType}\n${body}`)
-                // now that you've got a listener, do something with the result... 
-
-                // do something...
-
-            })
-
-            this.twitch.onVisibilityChanged((isVisible,_c)=>{
-                this.visibilityChanged(isVisible)
-            })
-
-            this.twitch.onContext((context,delta)=>{
-                this.contextUpdate(context,delta)
-            })
-        }
-    }
-
-    componentWillUnmount(){
-        if(this.twitch){
-            this.twitch.unlisten('broadcast', ()=>console.log('successfully unlistened'))
-        }
-    }
-   
-    doDamage(){
-        let {bossHP} = this.state;
-        bossHP -= 10;
-        if(bossHP > 0) {
-            this.setState({bossHP});
-        } else {
-            this.setState({dead: true});
-            setTimeout(()=>{
-                this.generateBoss();
-            }, 500);
-        }
-        this.setState({shake: true});      
-    }
-
-    generateBoss(){
-        const emotes = ['https://static-cdn.jtvnw.net/emoticons/v1/33/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/973/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/22639/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/86/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/4057/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/111700/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/4339/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/360/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/357/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/25/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/41/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/425618/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/58765/3.0', 'https://static-cdn.jtvnw.net/emoticons/v1/88/3.0'];
-        const bossAvatar = emotes[Math.floor(Math.random()*14)];
-        const bossMaxHP = this.state.bossMaxHP + 100;
-        const bossHP = bossMaxHP;
-        this.setState({bossAvatar, bossHP, bossMaxHP});
-    }
-
-    render(){
-
-        let currentAnimation = '';
-
-        if(this.state.dead){
-            currentAnimation = "raid-boss-avatar dead"
-        } else if(this.state.shake){
-            currentAnimation = "raid-boss-avatar shake";
-        } else {
-            currentAnimation = "raid-boss-avatar";
-        }
-
-        if(this.state.finishedLoading && this.state.isVisible){
-            return (
-                <div className="App raid-boss-container">
-                    <div className="raid-boss-header">
-                        {this.state.bossName}
-                    </div>
-                    <img onClick={this.doDamage} className={currentAnimation} onAnimationEnd={() => this.setState({shake: false, dead: false})} src={this.state.bossAvatar}/>
-                    <div className="raid-boss-hp">
-                        HP: {this.state.bossHP}
-                    </div>
-                </div>
-            )
-        }else{
-            return (
-                <div className="App">
-                </div>
-            )
-        }
-
-    }
 }
+
+const pet = require('./controllers/pet.js');
+
+const port = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI||'mongodb://localhost/pet-battle', { useNewUrlParser: true });
+
+const subscribeSubscriber = (msg, data) => {
+    console.log(msg, data);
+}
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://localhost.rig.twitch.tv:8080");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+pet(app);
+
+app.get('/levelup/:channelId', function(req,res) {
+    var channelId = req.params.channelId;
+    Pet.findOne({userId: channelId})
+        .then((broadcasterPet) => {
+            console.log("~~~~~~Leveling up pet for channel: " + channelId);
+            broadcasterPet.set({
+                level: broadcasterPet.level + 1,
+                health: (broadcasterPet.level + 1) * 100
+            });
+            return broadcasterPet.save().then((pet) => {
+                io.emit('levelup', pet);
+                return pet;
+            })
+        }).catch((err) => {
+            console.error(err);
+            return err;
+        })
+        .then(function(data){
+            res.end(JSON.stringify(data));
+        })
+});
+
+app.listen(port, () => {
+    console.log("Listening on:",port);
+})
+
+server.listen(8888, () => {
+    console.log("IO Listening on:8888");
+});
