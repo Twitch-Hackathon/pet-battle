e6ac55e5facf4a46a848eb9164c2b7d7d77aa706
Severiano Badajoz <sevebadajoz@gmail.com>
2018-10-27 18:19:06 -0700

    Connect the dots

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
-cont app = express();
+const app = express();
+
+const pet = require('/controllers/pet.js');
 
 const port = process.env.PORT || 5000;
+
+pet(app);
